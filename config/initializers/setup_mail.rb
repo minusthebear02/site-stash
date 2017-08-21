3426892f4d60a5be06b97e6ed413946324f85c7c
Mark Froehlich <mfroeh2@gmail.com>
2017-08-21 19:35:48 -0400

    removing bad things

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,13 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
+  address: 'smtp.mailgun.org',
   port: '587',
   authentication: :plain,
-  user_name: ENV["action_mailer_username"],
-  password: ENV["action_mailer_password"],
-  domain: 'heroku.com',
-  enable_starttls_auto: true
+  user_name: ENV["ACTION_MAILER_USERNAME"],
+  password: ENV["ACTION_MAILER_PASSWORD"],
+  domain: 'https://site-stash.herokuapp.com/',
+  enable_starttls_auto: true,
+  content_type: 'text/html'
 }
+
+ActionMailer::Base.raise_delivery_errors = true
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     resources :bookmarks, except: :show
   end
 
+  post :incoming, to: 'incoming#create'
+
   root 'topics#index'
 end
