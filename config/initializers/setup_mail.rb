5cd973a922abba369d7fde006edf132b4e200703
Mark Froehlich <mfroeh2@gmail.com>
2017-08-21 21:31:07 -0400

    sendgrid.net

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.org',
+  address: 'smtp.sendgrid.net',
   port: '587',
   authentication: :plain,
   user_name: ENV["ACTION_MAILER_USERNAME"],
