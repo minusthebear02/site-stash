d4417f8c763168305ac3364ebdf215eae84a92aa
Mark Froehlich <mfroeh2@gmail.com>
2017-08-21 20:07:09 -0400

    Fixed up the incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,7 +3,13 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    puts "INCOMING PARAMS HERE: #{params}"
-
-    head 200
+    user = User.find_by(email: params[:sender])
+    if user
+      topic = user.topics.find_or_create_by(title: params[:subject])
+      bookmark = topic.find_or_create_by(url: params['stripped-text'])
+      head 200
+    else
+      head 401
+    end
   end
+end
