ccab6a22ddefad2992427e6a7251d56e74f68ee0
Mark Froehlich <mfroeh2@gmail.com>
2017-08-25 00:08:17 -0400

    Likes: implemented

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,33 @@
+class LikesController < ApplicationController
+
+  def index
+  end
+
+  def create
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.build(bookmark: @bookmark)
+
+    authorize like
+
+    if like.save
+      redirect_to @bookmark.topic, notice: "Site was liked."
+    else
+      flash.now[:alert] = "There was a problem liking the site. Please try again."
+      redirect_to @bookmark.topic
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.find(params[:id])
+
+    authorize like
+
+    if like.destroy
+      redirect_to @bookmark.topic, notice: "Site was unliked."
+    else
+      flash.now[:alert] = "There was a problem unliking the site. Please try again."
+      redirect_to @bookmark.topic
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'likes/index'
+
   devise_for :users
 
   resources :topics do
-    resources :bookmarks, except: :show
+    resources :bookmarks, except: :show do
+      resources :likes, only: [:index, :create, :destroy]
+    end
   end
 
   post :incoming, to: 'incoming#create'
